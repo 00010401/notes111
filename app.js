bb5e0615807023e2d5e8a881e08d67b26c579ee5
00010401 <stud00010401@gmail.com>
2021-03-27 18:53:36 +0500

    create, note, detail, home pages

--- app.js
+++ app.js
@@ -1,2 +1,30 @@
 const express = require('express');
-const pug = require('pug');
+const app = express();
+
+app.set('view engine', 'pug')
+
+app.use('/static', express.static('public'))
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/create', (req, res) => {
+    res.render('create')
+})
+
+const notes = ['Some awesome title', 'Some awesome title']
+
+app.get('/notes', (req, res) => {
+    res.render('notes', { notes: notes })
+})
+
+app.get('/notes/detail', (req, res) => {
+    res.render('detail')
+})
+
+app.listen(8000, err => {
+    if (err) console.log(err)
+
+    console.log('Server is running on port 8000...')
+})

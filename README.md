a8648c799212c059534565f9a2058ca2e1ff5ac6
00010401 <stud00010401@gmail.com>
2021-03-29 17:00:00 +0500

    note app

---
+++ README.md
@@ -0,0 +1,28 @@
+# NOTE TAKING WEB APP
+
+### This web application allows users to create, edit, delete notes.
+
+## To run this web application locally, you must follow the steps indicated below:
+
+1. Clone this repository
+```bash
+git clone [repository link]
+```
+
+2. Install dependencies
+``bash
+cd [project folder]
+npm install
+```
+
+3. Run the application
+```bash
+node app
+```
+
+### Web application ependencies
+- express.js
+- pug.js
+
+### Web application repository on github
+[Link to github]
--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+
+const fs = require('fs')
 
 app.set('view engine', 'pug')
 
 app.use('/static', express.static('public'))
+app.use(express.urlencoded( { extended: false }))
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -13,14 +16,58 @@ app.get('/create', (req, res) => {
     res.render('create')
 })
 
-const notes = ['Some awesome title', 'Some awesome title']
+app.post('/create', (req, res) => {
+    const title = req.body.title
+    const description = req.body.description
 
-app.get('/notes', (req, res) => {
-    res.render('notes', { notes: notes })
+    if (title.trim() === '' && description.trim() === '') {
+        res.render('create', { error: true })
+    }
+    else{
+        fs.readFile('./data/notes.json', (err, data) => {
+            if (err) throw error;
+
+            const notes = JSON.parse(data)
+
+            notes.push({
+                id: id (),
+                title: title,
+                description: description
+            })
+
+            fs.writeFile('./data/notes.json', JSON.stringify(notes), err => {
+                if (err) throw err
+
+            res.render('create', { success: true })
+            })
+        })
+    }
 })
 
-app.get('/notes/detail', (req, res) => {
-    res.render('detail')
+app.get('/notes', (req, res) => {
+
+    fs.readFile('./data/notes.json', (err, data) => {
+        if (err) throw err
+
+        const notes = JSON.parse(data)
+
+        res.render('notes', { notes: notes })
+    })
+})
+
+app.get('/notes/:id', (req, res) => {
+    const id = req.params.id
+
+    fs.readFile('./data/notes.json', (err, data) => {
+        if (err) throw err
+
+        const notes = JSON.parse(data)
+
+        const note = notes.filter(note => note.id == id)[0]
+
+        res.render('detail', { note: note })
+    })
+   
 })
 
 app.listen(8000, err => {
@@ -28,3 +75,7 @@ app.listen(8000, err => {
 
     console.log('Server is running on port 8000...')
 })
+
+function id () {
+    return '_' + Math.random().toString(36).substr(2, 9);
+};
